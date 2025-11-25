84ce6d377026b037abc334778fb2b3e67317ba43
Trevor bercich <telltrevorhiscraftsareready@gmail.com>
2025-11-25 18:56:43 +0000

    okay i added the new files only change the one line of code you said you

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
